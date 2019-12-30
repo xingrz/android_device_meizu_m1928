89d72a61da78c72f82f7ea140c2fcc3011062ce7
XiNGRZ <chenxingyu92@gmail.com>
2019-12-30 13:38:28 +0800

    m1928: Bind mount to remove IBootControl from vendor manifest.xml
    The prebuilt vendor defined this HIDL in its manifest but didn't shipped
    it, causing the system infinitely waiting for it while booting.
    
    Change-Id: I6548bb3e9437cdf7a4d652ac5619ad16a913cb74

--- device.mk
+++ device.mk
@@ -11,5 +11,13 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-mokee
 
+# Init
+PRODUCT_PACKAGES += \
+    init.target.rc
+
+# Manifest
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/vendor_manifest.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/vendor_manifest.xml
+
 # Inherit from sm8150-common
 $(call inherit-product, device/meizu/sm8150-common/common.mk)
